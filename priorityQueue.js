e6e9773c5aafc49f30794fecf4b82007d5a193fa
Chloe Park <chloe@protopie.io>
2022-01-07 17:27:08 +0900

    Priority Queue

--- priorityQueue.js
+++ priorityQueue.js
@@ -1,14 +1,23 @@
-// MaxBinaryHeap -> parent nodes are always larger than child nodes
-// MinBinaryHeap -> parent nodes are always smaller than child nodes
-// a binary heap is as compact as possible
+// 각 요소가 그에 해당하는 우선순위를 가지는 데이터 구조
+// 서로 다른 우선순위를 가지는 데이터나 정보를 관리할 필요가 있거나,
+// 무언가를 입력하는데 순서에 맞추어 데이터를 입력하지 않거나, 응급실
+// 유닉스의 나이스
 
-class MaxBinaryHeap {
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
+class PriorityQueue {
   constructor() {
     this.values = [];
   }
 
-  insert(element) {
-    this.values.push(element);
+  enqueue(val, priority) {
+    const newNode = new Node(val, priority);
+    this.values.push(newNode);
     this.bubbleUp();
   }
 
@@ -19,7 +28,7 @@ class MaxBinaryHeap {
     while (idx > 0) {
       let parentIdx = Math.floor((idx - 1) / 2);
       let parent = this.values[parentIdx];
-      if (element <= parent) break;
+      if (element.priority >= parent.priority) break;
 
       this.values[parentIdx] = element;
       this.values[idx] = parent;
@@ -28,14 +37,14 @@ class MaxBinaryHeap {
     return this.values;
   }
 
-  extractMax() {
-    const max = this.values[0];
+  dequeue() {
+    const min = this.values[0];
     const end = this.values.pop();
     if (this.values.length > 0) {
       this.values[0] = end;
       this.sinkDown();
     }
-    return max;
+    return min;
   }
 
   sinkDown() {
@@ -51,13 +60,13 @@ class MaxBinaryHeap {
 
       if (leftChildIdx < length) {
         leftChild = this.values[leftChildIdx];
-        if (leftChild > element) swap = leftChildIdx;
+        if (leftChild.priority < element.priority) swap = leftChildIdx;
       }
 
       if (rightChildIdx < length) {
         rightChild = this.values[rightChildIdx];
-
-        if (rightChild > this.values[swap]) swap = rightChildIdx;
+        if (rightChild.priority < this.values[swap].priority)
+          swap = rightChildIdx;
       }
 
       if (swap === 0) break;
@@ -68,4 +77,4 @@ class MaxBinaryHeap {
   }
 }
 
-const heap = new MaxBinaryHeap();
+const PQ = new PriorityQueue();
